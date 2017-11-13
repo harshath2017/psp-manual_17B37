e73e57bb686e16eb31bd8bd55303c82f7c365534
harshath2017 <33338973+harshath2017@users.noreply.github.com>
2017-11-13 15:40:16 +0530

    Add files via upload

---
+++ select and sort 6.py
@@ -0,0 +1,33 @@
+def selectsort(L): 
+    for slot in range(len(L)-1, 0, -1):
+        positionOfMax = 0
+        for location in range(1, slot + 1):
+            if L[location] > L[positionOfMax]:
+                positionOfMax = location
+
+        temp             = L[slot]
+        L[slot]          = L[positionOfMax]
+        L[positionOfMax] = temp
+    return L
+
+
+
+
+
+
+def insertsort(L):
+
+  for index in range(1, len(L)):
+    key = L[index]
+    j = index - 1
+
+    while j >= 0 and (L[j] > key):
+      L[j + 1] = L[j]
+      j = j - 1
+    L[j + 1] = key
+  return L
+L = [12,32,45,23,37,67]
+print(selectsort(L))
+
+
+
